432ac195f488e1caea26c740f3bf94827757da14
dolphin323 <liza140620002@gmail.com>
2022-09-21 20:44:49 +0300

    feat: add lab1

---
+++ lab1/brus/tests/elizaveta-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m brus -p elizaveta [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "elizaveta"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
